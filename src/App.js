2ffcda88baa3468adb8afc5f6d1f9c8f7adbc95b
Victor Garcia Mestre <vgmestre@gmail.com>
2021-04-23 12:49:48 +0200

    fetch data function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+const url =
+  "https://api.covid19api.com/world?from=2021-04-01T00:00:00Z&to=2021-04-20T00:00:00Z";
+
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData(url);
+    console.log(data);
+  }
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
