7475547b4b7c5bdad80aff83121b9408ac57ed8c
Victor Garcia Mestre <vgmestre@gmail.com>
2021-04-23 22:31:37 +0200

    installed classnames module and added styles to Cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 import styles from "./Cards.module.css";
 import CountUp from "react-countup";
+import cx from "classnames";
 
 const Cards = ({ TotalConfirmed, TotalDeaths, TotalRecovered }) => {
   if (!TotalConfirmed) {
@@ -11,7 +12,13 @@ const Cards = ({ TotalConfirmed, TotalDeaths, TotalRecovered }) => {
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.infected)}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Infected
@@ -30,24 +37,50 @@ const Cards = ({ TotalConfirmed, TotalDeaths, TotalRecovered }) => {
             </Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.recovered)}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Recovered
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={TotalRecovered}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant="body2">
               Number of recoveries from COVID-19
             </Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.deaths)}
+        >
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Deaths
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={TotalDeaths}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant="body2">
               Number of deaths caused by COVID-19
