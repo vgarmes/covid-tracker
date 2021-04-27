465c950f92045078e3ca043b1285e7153fdf26ff
Victor Garcia Mestre <vgmestre@gmail.com>
2021-04-27 10:39:46 +0200

    chart params cleanup

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,94 +5,101 @@ import ChartPicker from "../ChartPicker/ChartPicker";
 
 const Chart = ({ dailyData }) => {
   const [showBarChart, setShowBarChart] = useState(false);
-  const changeChart = () => {
+  const switchChart = () => {
     setShowBarChart(!showBarChart);
   };
+
+  const lineChartData = [
+    {
+      labels: dailyData.map(({ Date }) => Date.slice(0, 10)),
+      datasets: [
+        {
+          data: dailyData.map(({ TotalConfirmed }) => TotalConfirmed),
+          label: "Infected",
+          borderColor: "#333ff",
+          fill: true,
+        },
+      ],
+    },
+    {
+      labels: dailyData.map(({ Date }) => Date.slice(0, 10)),
+      datasets: [
+        {
+          data: dailyData.map(({ TotalDeaths }) => TotalDeaths),
+          label: "Deaths",
+          borderColor: "red",
+          backgroundColor: "rgba(255, 0, 0, 0.5)",
+          fill: true,
+        },
+      ],
+    },
+  ];
+
+  const barChartData = [
+    {
+      labels: dailyData.map(({ Date }) => Date.slice(0, 10)),
+      datasets: [
+        {
+          label: "Infected",
+          data: dailyData.map(({ NewConfirmed }) => NewConfirmed),
+          backgroundColor: "rgb(51, 63, 15)",
+        },
+      ],
+    },
+    {
+      labels: dailyData.map(({ Date }) => Date.slice(0, 10)),
+      datasets: [
+        {
+          label: "Deaths",
+          data: dailyData.map(({ NewDeaths }) => NewDeaths),
+          backgroundColor: "rgb(255, 0, 0)",
+        },
+      ],
+    },
+  ];
+
+  const barChartOptions = [
+    {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+              display: false,
+            },
+          },
+        ],
+      },
+    },
+    {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  ];
+
   const lineChart = dailyData.length ? (
     <>
-      <Line
-        data={{
-          labels: dailyData.map(({ Date }) => Date.slice(0, 10)),
-          datasets: [
-            {
-              data: dailyData.map(({ TotalConfirmed }) => TotalConfirmed),
-              label: "Infected",
-              borderColor: "#333ff",
-              fill: true,
-            },
-          ],
-        }}
-      />
-      <Line
-        data={{
-          labels: dailyData.map(({ Date }) => Date.slice(0, 10)),
-          datasets: [
-            {
-              data: dailyData.map(({ TotalDeaths }) => TotalDeaths),
-              label: "Deaths",
-              borderColor: "red",
-              backgroundColor: "rgba(255, 0, 0, 0.5)",
-              fill: true,
-            },
-          ],
-        }}
-      />
+      <Line data={lineChartData[0]} />
+      <Line data={lineChartData[1]} />
     </>
   ) : null;
 
   const barChart = dailyData.length ? (
     <>
-      <Bar
-        data={{
-          labels: dailyData.map(({ Date }) => Date.slice(0, 10)),
-          datasets: [
-            {
-              label: "Infected",
-              data: dailyData.map(({ NewConfirmed }) => NewConfirmed),
-              backgroundColor: "rgb(51, 63, 15)",
-            },
-          ],
-        }}
-        options={{
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            ],
-          },
-        }}
-      />
-      <Bar
-        data={{
-          labels: dailyData.map(({ Date }) => Date.slice(0, 10)),
-          datasets: [
-            {
-              label: "Deaths",
-              data: dailyData.map(({ NewDeaths }) => NewDeaths),
-              backgroundColor: "rgb(255, 0, 0)",
-            },
-          ],
-        }}
-        options={{
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            ],
-          },
-        }}
-      />
+      <Bar data={barChartData[0]} options={barChartOptions[0]} />
+      <Bar data={barChartData[1]} options={barChartOptions[1]} />
     </>
   ) : null;
   return (
     <>
-      <ChartPicker showBarChart={showBarChart} changeChart={changeChart} />
+      <ChartPicker showBarChart={showBarChart} switchChart={switchChart} />
       <div className={styles.container}>
         {showBarChart ? barChart : lineChart}
       </div>
--- src/components/ChartPicker/ChartPicker.jsx
+++ src/components/ChartPicker/ChartPicker.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Switch, Typography } from "@material-ui/core";
 import styles from "./ChartPicker.module.css";
 
-const ChartPicker = ({ showBarChart, changeChart }) => {
+const ChartPicker = ({ showBarChart, switchChart }) => {
   return (
     <div className={styles.container}>
       <Typography variant="body2">Accumulated</Typography>
       <Switch
         checked={showBarChart}
-        onChange={() => changeChart()}
+        onChange={() => switchChart()}
         name="checked"
         inputProps={{ "aria-label": "secondary checkbox" }}
       />
