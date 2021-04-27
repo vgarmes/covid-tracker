13325e236a82322046d61178a938465a804094f6
Victor Garcia Mestre <vgmestre@gmail.com>
2021-04-28 00:12:21 +0200

    responsive charts

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -18,6 +18,8 @@ const Chart = ({ dailyData }) => {
           label: "Infected",
           borderColor: "#333ff",
           fill: true,
+          pointRadius: 0,
+          pointHitRadius: 20,
         },
       ],
     },
@@ -30,10 +32,38 @@ const Chart = ({ dailyData }) => {
           borderColor: "red",
           backgroundColor: "rgba(255, 0, 0, 0.5)",
           fill: true,
+          pointRadius: 0,
+          pointHitRadius: 20,
         },
       ],
     },
   ];
+
+  const lineChartOptions = {
+    onResize: function (chart, size) {
+      if (size.width < 450) {
+        chart.options.scales.x.ticks.display = false;
+        chart.options.scales.x.grid.display = false;
+        chart.update();
+      } else {
+        chart.options.scales.x.ticks.display = true;
+        chart.options.scales.x.grid.display = true;
+        chart.update();
+      }
+    },
+    scales: {
+      y: {
+        ticks: {
+          callback: function (value, index, values) {
+            if (value >= 1e6) {
+              return `${value / 1e6}M`;
+            }
+            return value;
+          },
+        },
+      },
+    },
+  };
 
   const barChartData = [
     {
@@ -58,43 +88,46 @@ const Chart = ({ dailyData }) => {
     },
   ];
 
-  const barChartOptions = [
-    {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-              display: false,
-            },
+  const barChartOptions = {
+    onResize: function (chart, size) {
+      console.log(size);
+      if (size.width < 450) {
+        chart.options.scales.x.ticks.display = false;
+        chart.options.scales.x.grid.display = false;
+        chart.update();
+      } else {
+        chart.options.scales.x.ticks.display = true;
+        chart.options.scales.x.grid.display = true;
+        chart.update();
+      }
+    },
+    scales: {
+      y: {
+        ticks: {
+          callback: function (value, index, values) {
+            if (value >= 1e6) {
+              return `${value / 1e6}M`;
+            } else if (value >= 1e3) {
+              return `${value / 1e3}K`;
+            }
+            return value;
           },
-        ],
+        },
       },
     },
-    {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-            },
-          },
-        ],
-      },
-    },
-  ];
+  };
 
   const lineChart = dailyData.length ? (
     <>
-      <Line data={lineChartData[0]} />
-      <Line data={lineChartData[1]} />
+      <Line data={lineChartData[0]} options={lineChartOptions} />
+      <Line data={lineChartData[1]} options={lineChartOptions} />
     </>
   ) : null;
 
   const barChart = dailyData.length ? (
     <>
-      <Bar data={barChartData[0]} options={barChartOptions[0]} />
-      <Bar data={barChartData[1]} options={barChartOptions[1]} />
+      <Bar data={barChartData[0]} options={barChartOptions} />
+      <Bar data={barChartData[1]} options={barChartOptions} />
     </>
   ) : null;
   return (
